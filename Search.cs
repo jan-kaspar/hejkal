f54a6816d718bf29b3fa6848f30ffb1a6be2302c
Jan Kašpar <jan.kaspar@edhouse.cz>
2022-05-26 08:13:20 +0200

    search: complete numbers, case insesitive for titles

--- Search.cs
+++ Search.cs
@@ -1,1 +1,50 @@
-﻿using System.Collections.Generic;namespace Hejkal{	struct SongData	{		string number;		string title;		public SongData(string _n, string _t)		{			number = _n;			title = _t;		}		public override string ToString()		{			return number + ": " + title;		}	}	partial class Search	{		private List<SongData> allSongs;		public string[] FindSongs(string pattern)		{			List<string> results = new List<string>();			foreach (var song in allSongs)			{				var song_str = song.ToString();				if (song_str.Contains(pattern))					results.Add(song_str);			}			return results.ToArray();		}	}}
+﻿using System.Collections.Generic;
+
+namespace Hejkal
+{
+	struct SongData
+	{
+		string number;
+		string title;
+
+		public SongData(string _n, string _t)
+		{
+			number = _n;
+			title = _t;
+		}
+
+		public override string ToString()
+		{
+			return number + ": " + title;
+		}
+
+		public bool Match(string key)
+		{
+			if (number == key)
+				return true;
+
+			if (title.IndexOf(key, System.StringComparison.OrdinalIgnoreCase) >= 0)
+				return true;
+
+			return false;
+		}
+	}
+
+	partial class Search
+	{
+		private List<SongData> allSongs;
+
+		public string[] FindSongs(string pattern)
+		{
+			List<string> results = new List<string>();
+
+			foreach (var song in allSongs)
+			{
+				if (song.Match(pattern))
+					results.Add(song.ToString());
+			}
+
+			return results.ToArray();
+		}
+	}
+}
