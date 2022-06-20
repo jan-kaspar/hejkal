770619f1274c524ea736abeab06621eb998ff2cd
Jan Kašpar <jan.kaspar@edhouse.cz>
2022-06-20 14:00:54 +0200

    empty MAUI application

---
+++ Hejkal/Search.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Hejkal
+{
+	partial class Search
+	{
+		private List<SongData> allSongs;
+
+		public List<SongData> FindSongs(string pattern)
+		{
+			List<SongData> results = new List<SongData>();
+
+			foreach (var song in allSongs)
+			{
+				if (song.Match(pattern))
+					results.Add(song);
+			}
+
+			// TODO: remove when testing over
+			if (pattern == "test")
+				results.Add(new SongData("test", "test"));
+
+			return results;
+		}
+
+		public List<SongData> GetAllSongs() => allSongs;
+	}
+}
